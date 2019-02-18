1cd776cb1689429ef6fdb49df3097d81b1304256
isaacs <i@izs.me>
2019-02-18 12:18:15 -0800

    Restrict lockfile and core dump exclusions
    Do not ignore directories named 'core'.
    
    Only ignore package-lock.json and yarn.lock at the root of the package.
    
    Fix #24
    Fix #18

---
+++ test/nested-lock-and-core.js
@@ -0,0 +1,61 @@
+'use strict'
+const fs = require('fs')
+const path = require('path')
+
+const mkdirp = require('mkdirp')
+const rimraf = require('rimraf')
+const t = require('tap')
+
+const pack = require('../')
+
+const pkg = path.join(__dirname, path.basename(__filename, '.js'))
+t.teardown(_ => rimraf.sync(pkg))
+
+t.test('setup', t => {
+  rimraf.sync(pkg)
+  mkdirp.sync(pkg + '/lib')
+  mkdirp.sync(pkg + '/core')
+  fs.writeFileSync(pkg + '/lib/core', 'excluded dump file')
+  fs.writeFileSync(pkg + '/package.json', JSON.stringify({
+    name: 'test-package',
+    version: '1.2.3',
+  }))
+  fs.writeFileSync(pkg + '/package-lock.json', JSON.stringify({
+    lock: 'file',
+    include: false,
+  }))
+  fs.writeFileSync(pkg + '/yarn.lock', JSON.stringify({
+    lock: 'file',
+    include: false,
+  }))
+  fs.writeFileSync(pkg + '/lib/package-lock.json', JSON.stringify({
+    lock: 'file',
+    include: true,
+  }))
+  fs.writeFileSync(pkg + '/lib/yarn.lock', JSON.stringify({
+    lock: 'file',
+    include: true,
+  }))
+  fs.writeFileSync(pkg + '/core/include-me.txt', 'please include me')
+  t.end()
+})
+
+
+t.test('follows npm package ignoring rules', function (t) {
+  const expect = [
+    'package.json',
+    'core/include-me.txt',
+    'lib/package-lock.json',
+    'lib/yarn.lock',
+  ]
+
+  const check = (files, t) => {
+    t.same(files, expect)
+    t.end()
+  }
+
+  t.test('sync', t => check(pack.sync({ path: pkg }), t))
+  t.test('async', t => pack({ path: pkg }).then(files => check(files, t)))
+
+  t.end()
+})
