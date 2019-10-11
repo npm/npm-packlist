13f699cf0ca66b4de29e70cf357b1889e2c4bf11
isaacs <i@izs.me>
2019-10-11 16:01:17 -0700

    Add bin script
    Close: https://github.com/npm/npm-packlist/issues/12

---
+++ tap-snapshots/test-bin.js-TAP.test.js
@@ -0,0 +1,42 @@
+/* IMPORTANT
+ * This snapshot file is auto-generated, but designed for humans.
+ * It should be checked into source control and tracked carefully.
+ * Re-generate by setting TAP_SNAPSHOT=1 and running tests.
+ * Make sure to inspect the output below.  Do not ignore changes!
+ */
+'use strict'
+exports[`test/bin.js TAP --sort > expect resolving Promise 1`] = `
+Object {
+  "code": 0,
+  "signal": null,
+  "stderr": "",
+  "stdout": "index.js\\nlib/index.js\\nLICENSE.txt\\npackage.json\\nREADME.md\\n",
+}
+`
+
+exports[`test/bin.js TAP -s > expect resolving Promise 1`] = `
+Object {
+  "code": 0,
+  "signal": null,
+  "stderr": "",
+  "stdout": "index.js\\nlib/index.js\\nLICENSE.txt\\npackage.json\\nREADME.md\\n",
+}
+`
+
+exports[`test/bin.js TAP dir argument > expect resolving Promise 1`] = `
+Object {
+  "code": 0,
+  "signal": null,
+  "stderr": "",
+  "stdout": "> .\\nindex.js\\nlib/index.js\\npackage.json\\nREADME.md\\nLICENSE.txt\\n",
+}
+`
+
+exports[`test/bin.js TAP no args > expect resolving Promise 1`] = `
+Object {
+  "code": 0,
+  "signal": null,
+  "stderr": "",
+  "stdout": "index.js\\nlib/index.js\\npackage.json\\nREADME.md\\nLICENSE.txt\\n",
+}
+`
---
+++ test/bin.js
@@ -0,0 +1,39 @@
+const {spawn} = require('child_process')
+const bin = require.resolve('../bin/index.js')
+const run = (...args) => new Promise(res => {
+  const out = []
+  const err = []
+  const c = spawn(process.execPath, [bin, ...args], { cwd: me })
+  c.stdout.on('data', c => out.push(c))
+  c.stderr.on('data', c => err.push(c))
+  c.on('close', (code, signal) => res({
+    code,
+    signal,
+    stdout: Buffer.concat(out).toString('utf8'),
+    stderr: Buffer.concat(err).toString('utf8'),
+  }))
+})
+const t = require('tap')
+const {resolve, basename} = require('path')
+const me = resolve(__dirname, basename(__filename, '.js'))
+const rimraf = require('rimraf')
+const mkdirp = require('mkdirp')
+rimraf.sync(me)
+mkdirp.sync(me)
+t.teardown(() => rimraf.sync(me))
+
+const fs = require('fs')
+fs.writeFileSync(me + '/package.json', JSON.stringify({
+  files: ['index.js', 'lib']
+}))
+fs.writeFileSync(me + '/README.md', 'hello')
+fs.writeFileSync(me + '/LICENSE.txt', 'you can use it but you gotta pay me')
+fs.writeFileSync(me + '/index.js', 'console.log(/xss/)')
+mkdirp.sync(me + '/lib')
+fs.writeFileSync(me + '/lib/index.js', 'console.log(/xss/)')
+fs.writeFileSync(me + '/ignore.js', 'throw new Error("dont look at me!")')
+
+t.test('no args', t => t.resolveMatchSnapshot(run()))
+t.test('--sort', t => t.resolveMatchSnapshot(run('--sort')))
+t.test('-s', t => t.resolveMatchSnapshot(run('-s')))
+t.test('dir argument', t => t.resolveMatchSnapshot(run('.')))
