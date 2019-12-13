7286246e7886e2a62cde4c5ec22db450bdc96f12
Daniel Stockman <daniel.stockman@gmail.com>
2019-12-13 14:07:46 -0800

    fix: Respect root manifest provided in opts.packageJsonCache
    PR-URL: https://github.com/npm/npm-packlist/pull/47
    Credit: @evocateur
    Close: #47
    Reviewed-by: @isaacs

---
+++ tap-snapshots/test-package-json-cache.js-TAP.test.js
@@ -0,0 +1,34 @@
+/* IMPORTANT
+ * This snapshot file is auto-generated, but designed for humans.
+ * It should be checked into source control and tracked carefully.
+ * Re-generate by setting TAP_SNAPSHOT=1 and running tests.
+ * Make sure to inspect the output below.  Do not ignore changes!
+ */
+'use strict'
+exports[`test/package-json-cache.js TAP seeded with invalid JSON falls back to filesystem > must match snapshot 1`] = `
+Array [
+  "quendi.js",
+  "package.json",
+]
+`
+
+exports[`test/package-json-cache.js TAP seeded with root manifest > expect resolving Promise 1`] = `
+Array [
+  "elf.js",
+  "package.json",
+]
+`
+
+exports[`test/package-json-cache.js TAP seeded with root manifest > must match snapshot 1`] = `
+Array [
+  "elf.js",
+  "package.json",
+]
+`
+
+exports[`test/package-json-cache.js TAP when empty > must match snapshot 1`] = `
+Array [
+  "quendi.js",
+  "package.json",
+]
+`
