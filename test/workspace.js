da1ba4a3051ea822d9625347e00c186677247a45
nlf <quitlahok@gmail.com>
2022-05-31 13:04:31 -0700

    fix: correctly ignore .gitignore when a .npmignore is present (#108)

--- test/workspace.js
+++ test/workspace.js
@@ -109,3 +109,67 @@ t.test('packing a workspace root does not include children', async (t) => {
     'workspaces/foo/package.json',
   ])
 })
+
+t.test('.gitignore is discarded if .npmignore exists outside of tree', async (t) => {
+  const root = t.testdir({
+    'package.json': JSON.stringify({
+      name: 'workspace-root',
+      version: '1.0.0',
+      main: 'root.js',
+      workspaces: ['./workspaces/foo'],
+    }),
+    'root.js': `console.log('hello')`,
+    '.gitignore': 'dont-ignore-me',
+    '.npmignore': 'only-ignore-me',
+    'dont-ignore-me': 'should not be ignored',
+    'only-ignore-me': 'should be ignored',
+    workspaces: {
+      '.gitignore': 'dont-ignore-me-either',
+      '.npmignore': 'ignore-me-also',
+      'dont-ignore-me': 'should not be ignored',
+      'dont-ignore-me-either': 'should not be ignored',
+      'only-ignore-me': 'should be ignored',
+      'ignore-me-also': 'should be ignored',
+      foo: {
+        'package.json': JSON.stringify({
+          name: 'workspace-child',
+          version: '1.0.0',
+          main: 'child.js',
+        }),
+        'child.js': `console.log('hello')`,
+        'dont-ignore-me': 'should not be ignored',
+        'dont-ignore-me-either': 'should not be ignored',
+        'only-ignore-me': 'should be ignored',
+        'ignore-me-also': 'should also be ignored',
+      },
+    },
+  })
+
+  const workspacePath = path.join(root, 'workspaces', 'foo')
+  // this simulates what it looks like when a user does i.e. npm pack -w ./workspaces/foo
+  const files = await packlist({
+    path: workspacePath,
+    prefix: root,
+    workspaces: [workspacePath],
+  })
+  t.same(files, [
+    'dont-ignore-me',
+    'dont-ignore-me-either',
+    'child.js',
+    'package.json',
+  ])
+
+  // here we leave off workspaces to satisfy coverage
+  const secondFiles = await packlist({
+    path: workspacePath,
+    prefix: root,
+  })
+  t.same(secondFiles, [
+    'dont-ignore-me',
+    'dont-ignore-me-either',
+    'ignore-me-also',
+    'only-ignore-me',
+    'child.js',
+    'package.json',
+  ])
+})
