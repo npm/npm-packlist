b09f47fe3bd031d5692b97457340187751d77dbb
Luke Karrys <luke@lukekarrys.com>
2022-12-06 18:22:05 -0700

    wip: failing test

---
+++ test/package-json-directory-glob.js
@@ -0,0 +1,41 @@
+'use strict'
+
+const Arborist = require('@npmcli/arborist')
+const t = require('tap')
+const packlist = require('..')
+
+const createTestdir = (...files) => t.testdir({
+  'package.json': JSON.stringify({
+    files,
+  }),
+  folder: {
+    one: { file: 'one' },
+    two: { file: 'two' },
+  },
+  folder1: {
+    one: { file: 'one' },
+    two: { file: 'two' },
+  },
+})
+
+t.test('package json directory glob', async (t) => {
+  const pkgFiles = [
+    'folder/',
+    'folder/*',
+    'folder/**',
+  ]
+
+  for (const files of pkgFiles) {
+    await t.test(files, async t => {
+      const pkg = createTestdir(files)
+      const arborist = new Arborist({ path: pkg })
+      const tree = await arborist.loadActual()
+      const res = await packlist(tree)
+      t.same(res, [
+        'folder/one/file',
+        'folder/two/file',
+        'package.json',
+      ])
+    })
+  }
+})
