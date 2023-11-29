7b4165e60468e558a8b91f40c20761dcbe47d11d
Mohamed Akram <mohd.akram@outlook.com>
2023-11-29 23:27:28 +0400

    fix: preserve slashes in specified files

--- test/package-json-dir-with-slashes.js
+++ test/package-json-dir-with-slashes.js
@@ -13,6 +13,7 @@ const pkg = t.testdir({
     files: [
       '/lib',
       './lib2',
+      './lib3/*',
     ],
   }),
   lib: {
@@ -26,6 +27,7 @@ const pkg = t.testdir({
     'fiv.js': 'fiv',
     '.DS_Store': 'a store of ds',
   },
+  lib3: 'not a dir',
 })
 
 t.test('package with slash directories', async (t) => {
