3bad7f275b9d086e112e0efea4ad6d725b4998eb
Gar <gar+gh@danger.computer>
2023-12-06 08:45:31 -0800

    fix: always ignore .npmrc files at every level

--- test/ignore-file-included-by-globstar.js
+++ test/ignore-file-included-by-globstar.js
@@ -4,9 +4,45 @@ const Arborist = require('@npmcli/arborist')
 const t = require('tap')
 const packlist = require('../')
 
+t.test('exclude certain files always', async t => {
+  const path = t.testdir({
+    '.npmrc': 'secrets=true',
+    '.git': {
+      HEAD: 'empty',
+    },
+    node_modules: {
+      foo: {
+        'index.js': '',
+      },
+    },
+    subdir: {
+      'other.js': '',
+      '.npmrc': 'sneaky=true',
+    },
+    'index.js': '',
+    'glorp.txt': '',
+    'package.json': JSON.stringify({
+      name: '@npmcli/globstar-test',
+      version: '1.0.0',
+      files: ['*'],
+    }),
+    'package-lock.json': '{}',
+    'yarn.lock': '{}',
+    'pnpm-lock.yaml': '{}',
+  })
+  const arborist = new Arborist({ path })
+  const tree = await arborist.loadActual()
+  const files = await packlist(tree)
+  t.same(files, [
+    'index.js',
+    'subdir/other.js',
+    'package.json',
+    'glorp.txt',
+  ])
+})
+
 t.test('include a globstar, then exclude one of them', async (t) => {
   const path = t.testdir({
-    'foo.js': '',
     'bar.js': '',
     bar: {
       'bar.js': '',
